b1ec5740af05b8eaaa46f901934c02d2e3270e88
feiye-sunjingru <1050963440@qq.com>
2026-03-02 11:02:03 +0800

    perf(异步编程流程）：
    + asyncio.gather 聚合并发协程函数，这些函数并发执行
    + asyncio.run 创建eventloop, 执行main函数
    + print操作本身是同步的：因此10先输出

--- test_async_await_perf.py
+++ test_async_await_perf.py
@@ -17,22 +17,34 @@ async def task2():
 # 定义一个协程函数：表明交给EventLoop执行
 async def main():
     print('开始执行任务...')
+
+    """
     event_loop = asyncio.get_running_loop()
     # task1()是一个协程对象：创建一个Task对象，交给EventLoop调度执行
     t1 = event_loop.create_task(task1())
     t2 = event_loop.create_task(task2())
 
     result = await t1
-    # 10先输出，是因为print 是同步操作
     print('任务1执行结果:', result)
     result = await t2
     print('任务2执行结果:', result)
+    """
+
+    # 等价上面注释掉的代码：创建Task对象，交给EventLoop调度执行，等待结果
+    result = await asyncio.gather(task1(), task2())
+    print('任务1执行结果:', result[0])
+    print('任务2执行结果:', result[1])
+
 
 if __name__ == '__main__':
     start = time.time()
+    """
     # 创建一个EventLoop
     event_loop = asyncio.get_event_loop()
     # 启动EventLoop，执行main函数：整个任务的执行入口
     event_loop.run_until_complete(main())
+    """
+    # 跟上面三行等价：创建一个EventLoop，启动EventLoop，执行main函数
+    asyncio.run(main())
 
     print('总耗时:', time.time() - start)
