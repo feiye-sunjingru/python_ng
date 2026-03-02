6a82135a7997add9211c014ebf8475868f2143ec
feiye-sunjingru <1050963440@qq.com>
2026-03-02 09:56:04 +0800

    feat(同步编程）：遇到IO任务仍需占用资源

---
+++ test_sync.py
@@ -0,0 +1,20 @@
+import time
+
+def task1():
+    time.sleep(5)  # 模拟一个耗时 5 秒的 I/O 操作
+    return 10
+
+def task2():
+    time.sleep(3)  # 模拟一个耗时 3 秒的 I/O 操作
+    return 20
+
+def main():
+    result = task1()
+    print('任务1执行结果:', result)
+    result = task2()
+    print('任务2执行结果:', result)
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    print('总耗时:', time.time() - start)
