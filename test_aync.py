03058db33b2b8285135efcce6f4e66eee5a47fc9
feiye-sunjingru <1050963440@qq.com>
2026-03-02 11:14:50 +0800

    feat(async def作用): 定义协程对象
    + 协程对象：可交由EventLoop统一调度，遇到耗时IO操作及时让出CPU，该操作完成还能恢复CPU执行
    + 协程对象结果：必须由await进行获取
    + 协程对象执行：必须交由eventLoop

---
+++ test_aync.py
@@ -0,0 +1,15 @@
+import asyncio
+
+async def task():
+    print('task')
+
+async def demo():
+    # 协程函数，执行结果需要通过await获取
+    coro = await task()
+    # task()是一个协程对象，必须通过await获取结果
+    print(type(task()))
+    print(type(coro))
+
+if __name__ == '__main__':
+    # 协程对象必须通过event_loop才能执行
+    asyncio.run(demo())
