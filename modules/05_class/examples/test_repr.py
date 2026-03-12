09ab92e66cc4823fcc1f27d94bf2a734b45985f6
feiye-sunjingru <1050963440@qq.com>
2026-03-12 15:50:03 +0800

    feat(repr): 对象官方字符串表示
    + __repr__ 返回对象的"官方"字符串表示，优先用于调试和开发
    + 理想情况下，repr() 输出应是可执行代码（ eval(repr(obj)) == obj ）
    + 与 __str__ 区别：__repr__ 面向开发者，__str__ 面向用户

---
+++ modules/05_class/examples/test_repr.py
@@ -0,0 +1,25 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+p = Point(1, 2)
+# 优先使用__str__:如果没有定义__str__，则使用__repr__
+print(p)
+print(repr(p))  # 输出: Point(1, 2)
+
+print(repr(42))
+print(repr("hello"))
+print(repr([1, 2, 3]))
+
+# eval 用于执行字符串形式的代码，存在严重的安全风险（如代码注入）
+print(eval(repr(42)))
+print(eval(repr("hello")))
+print(eval(repr([1, 2, 3])))
