2129423741d0627c80f8b76ddbe63ddb136d88c7
feiye-sunjingru <1050963440@qq.com>
2026-03-20 11:21:07 +0800

    fix: 增加pandas类型存根、sys.exit()危险操作说明

--- modules/05_class/examples/test_del_notuse.py
+++ modules/05_class/examples/test_del_notuse.py
@@ -10,8 +10,9 @@ class Resource:
         # 假设这里要关闭文件、网络连接等
 
 
-r = Resource("my_resource")
+if __name__ == "__main__":
+    r = Resource("my_resource")
 
-# 强制退出: Python 解释器开始关闭,模块和全局变量会被清理，
-# 但此时很多内置对象（如 print、sys）可能已经被销毁, 因此可能无输出
-sys.exit()
+    # 强制退出: Python 解释器开始关闭,模块和全局变量会被清理，
+    # 但此时很多内置对象（如 print、sys）可能已经被销毁, 因此可能无输出
+    sys.exit()
